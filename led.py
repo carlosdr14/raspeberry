b8eb00f27b17100b49b77f65e4a3cb312b7f74a9
Carlos Duron <carlosduron973@gmail.com>
2023-03-03 10:04:04 -0600

    cambio

--- led.py
+++ led.py
@@ -1,35 +1,44 @@
 import RPi.GPIO as GPIO
 import time
 
-class Led:
+class LED:
     def __init__(self, pin):
         self.pin = pin
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.pin, GPIO.OUT)
-
-    def encender(self):
-        GPIO.output(self.pin, GPIO.HIGH)
-        print("LED encendido")
-
-    def apagar(self):
         GPIO.output(self.pin, GPIO.LOW)
-        print("LED apagado")
+    
+    def on(self):
+        GPIO.output(self.pin, GPIO.HIGH)
+    
+    def off(self):
+        GPIO.output(self.pin, GPIO.LOW)
+    
+    def cleanup(self):
+        GPIO.cleanup(self.pin)
 
-    def parpadear(self, duracion_encendido, duracion_apagado, repeticiones):
-        for i in range(repeticiones):
-            self.encender()
-            time.sleep(duracion_encendido)
-            self.apagar()
-            time.sleep(duracion_apagado)
+# Crea un objeto LED para el pin 11
+led = LED(18)
 
-    def limpiar(self):
-        GPIO.cleanup()
+try:
+    while True:
+        # Preguntar al usuario si desea prender o apagar el LED
+        action = input("Ingrese 'on' para prender el LED, 'off' para apagarlo, o 'exit' para salir: ")
+        
+        # Ejecutar la acción correspondiente
+        if action == "on":
+            led.on()
+        elif action == "off":
+            led.off()
+        elif action == "exit":
+            break
+        else:
+            print("Acción no válida.")
+    
+except KeyboardInterrupt:
+    pass
 
-# Ejemplo de uso
-led = Led(18)
-led.encender()
-time.sleep(5)
-led.apagar()
-led.parpadear(0.5, 0.5, 5)
-led.limpiar()
+# Limpiar el pin del LED antes de salir
+led.cleanup()
+
+
