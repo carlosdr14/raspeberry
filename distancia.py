41ed68b9fb3509af3caa85808a6ef6256b8c2541
Carlos Duron <carlosduron973@gmail.com>
2023-03-03 10:18:34 -0600

    pull

--- distancia.py
+++ distancia.py
@@ -1,54 +1,50 @@
-#Libraries
 import RPi.GPIO as GPIO
 import time
- 
-#GPIO Mode (BOARD / BCM)
-GPIO.setmode(GPIO.BCM)
+
+# Configuración de los pines GPIO
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
- 
-#set GPIO Pins
-GPIO_TRIGGER = 18
-GPIO_ECHO = 24
- 
-#set GPIO direction (IN / OUT)
+GPIO_TRIGGER = 11
+GPIO_ECHO = 13
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
- 
-def distance():
-    # set Trigger to HIGH
+
+def distancia():
+    # Poner el trigger en bajo
+    GPIO.output(GPIO_TRIGGER, False)
+
+    # Esperar un momento para permitir que el sensor se estabilice
+    time.sleep(0.5)
+
+    # Enviar un pulso al sensor
     GPIO.output(GPIO_TRIGGER, True)
- 
-    # set Trigger after 0.01ms to LOW
     time.sleep(0.00001)
     GPIO.output(GPIO_TRIGGER, False)
- 
-    StartTime = time.time()
-    StopTime = time.time()
- 
-    # save StartTime
+
+    # Esperar a que el sensor envíe una señal de retorno
     while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
- 
-    # save time of arrival
+        start_time = time.time()
+
     while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
- 
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
- 
+        end_time = time.time()
+
+    # Calcular la duración del pulso
+    pulse_duration = end_time - start_time
+
+    # Calcular la distancia en base a la duración del pulso
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
+
     return distance
- 
-if __name__ == '__main__':
-    try:
-        while True:
-            dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
- 
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
+
+# Ejemplo de uso de la función distancia
+try:
+    while True:
+        print("Distancia:", distancia(), "cm")
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    pass
+
+# Limpieza de los pines GPIO
+GPIO.cleanup()
