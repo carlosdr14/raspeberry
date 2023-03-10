1c2f0ae680b1ab2affa2923bbca2de33bc745153
Carlos Duron <carlosduron973@gmail.com>
2023-03-10 12:28:20 -0600

    xzzz

--- humedad.py
+++ humedad.py
@@ -1,36 +1,24 @@
 import Adafruit_DHT
 import time
 
-class TemperatureSensor:
-    def __init__(self, sensor, pin):
-        self.sensor = sensor
+class HumiditySensor:
+    def __init__(self, pin=17):
+        self.sensor = Adafruit_DHT.DHT11
         self.pin = pin
 
-    def get_temperature_humidity(self):
+    def get_humidity_temperature(self):
         humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.pin)
-        if temperature is not None and humidity is not None:
-            data = {'temperature': temperature, 'humidity': humidity}
-            return data
+        if humidity is not None and temperature is not None:
+            return humidity, temperature
         else:
-            return None
-
-class SensorMonitor:
-    def __init__(self, sensor, pin):
-        self.sensor = TemperatureSensor(sensor, pin)
+            return 0, 0
 
     def monitor(self, duration):
         start_time = time.time()
-        end_time = start_time + duration
-        while time.time() < end_time:
-            data = self.sensor.get_temperature_humidity()
-            if data is not None:
-                print("Temperature: {}C, Humidity: {}%".format(data['temperature'], data['humidity']))
-            else:
-                print("Failed to retrieve data.")
+        while (time.time() - start_time) < duration:
+            humidity, temperature = self.get_humidity_temperature()
+            print("Humidity: {:.1f}%, Temperature: {:.1f}°C".format(humidity, temperature))
             time.sleep(1)
 
-if __name__ == '__main__':
-    sensor = Adafruit_DHT.DHT11
-    pin = 17
-    monitor = SensorMonitor(sensor, pin)
-    monitor.monitor(10)
+sensor = HumiditySensor()
+sensor.monitor(60)  # Mide la humedad y la temperatura durante 60 segundos
