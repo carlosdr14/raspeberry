9bb789b3782b4d22d1ebe290ff71fcb79f353d27
nadia356 <110473332+nadia356@users.noreply.github.com>
2023-03-10 12:42:45 -0600

    jj

--- humedad.py
+++ humedad.py
@@ -1,15 +1,12 @@
-import Adafruit_DHT
-
-# set up sensor
-sensor = Adafruit_DHT.DHT22
-pin = 17
-
-# read data from sensor
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
-# trigger sensor
-if humidity is not None and temperature is not None:
-    dht22.trigger()
-    print('Temperature={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
+import Adafruit_DHT  
+import time  
+ 
+while True:
+  sensor = Adafruit_DHT.DHT11 #Cambia por DHT22 y si usas dicho sensor
+  pin = 17 #Pin en la raspberry donde conectamos el sensor
+  humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
+ 
+  print ('Humedad: ' , humedad)
+  print ('Temperatura: ' , temperatura)
+  
+  time.sleep(1) #Cada segundo se evalúa el sensor
