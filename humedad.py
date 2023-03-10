c2d5075afeeafb6f1392156a3ebee59baeabd9f0
Carlos Duron <carlosduron973@gmail.com>
2023-03-10 12:07:53 -0600

    22

--- humedad.py
+++ humedad.py
@@ -1,10 +1,28 @@
 import Adafruit_DHT
+import time
 
-class temperatura:
-    def __init__(self, pin):
-        self.sensor = Adafruit_DHT.DHT11
+class SensorTH:
+    def __init__(self, sensor, pin):
+        self.sensor = sensor
         self.pin = pin
+    
+    def get_temperature_humidity(self):
+        humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.pin)
+        if humidity is not None and temperature is not None:
+            return round(temperature, 1), round(humidity, 1)
+        else:
+            return None
+    
+    def monitor(self, interval):
+        print("Monitoring temperature and humidity...")
+        while True:
+            data = self.get_temperature_humidity()
+            if data is not None:
+                temperature, humidity = data
+                print("Temperature: {}°C, Humidity: {}%".format(temperature, humidity))
+            else:
+                print("Failed to retrieve data from sensor.")
+            time.sleep(interval)
 
-    def lectura(self):
-        hum, temp = Adafruit_DHT.read(self.sensor, self.pin)
-        return  hum, temp
+sensor = SensorTH(Adafruit_DHT.DHT11, 22)
+sensor.monitor(5)
