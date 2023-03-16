5867a8fe40474673863a38da68ac3472a6202cf3
nadia356 <110473332+nadia356@users.noreply.github.com>
2023-03-15 20:38:46 -0600

    humedadNS

--- humedad.py
+++ humedad.py
@@ -1,43 +1,19 @@
 import Adafruit_DHT
-
-# Configuración del sensor
-sensor = Adafruit_DHT.DHT22
-pin = 4
-
-# Lectura de los valores de humedad y temperatura
-humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
-
-# Mostrar los valores de humedad y temperatura
-if humedad is not None and temperatura is not None:
-    print('Temperatura={0:0.1f}°C Humedad={1:0.1f}%'.format(temperatura, humedad))
-else:
-    print('Error al leer los valores. Intenta de nuevo!')
-
-
-#Libraries
-import RPi.GPIO as GPIO
 import time
-import Adafruit_DHT
- 
-#GPIO Mode (BOARD / BCM)
-GPIO.setmode(GPIO.BCM)
- 
-#set GPIO Pins
-DHT_PIN = 17
- 
-def read_sensor():
-    # read sensor data
-    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, DHT_PIN)
- 
-    return temperature, humidity
- 
-try:
-     while True:
-      temperature, humidity = read_sensor()
-      print("Temperature = {:.1f}°C, Humidity = {:.1f}%".format(temperature, humidity))
-      time.sleep(2)
- 
-# Reset by pressing CTRL + C
-except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
+
+# Configura el pin del sensor
+sensor = Adafruit_DHT.DHT11
+pin = 5
+
+while True:
+    # Lee la temperatura y humedad del sensor
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+    # Si se lee un valor válido, muestra la temperatura y humedad en la consola
+    if humidity is not None and temperature is not None:
+        print(f"Temperatura = {temperature:.1f}°C  Humedad = {humidity:.1f}%")
+    else:
+        print("Error al leer el sensor")
+
+    # Espera 2 segundos antes de leer nuevamente el sensor
+    time.sleep(2)
