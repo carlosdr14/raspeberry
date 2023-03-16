226c8ac12758a23e66cd0e7fece9a323071bd69c
nadia356 <110473332+nadia356@users.noreply.github.com>
2023-03-15 20:41:58 -0600

    hNS

--- humedad.py
+++ humedad.py
@@ -3,4 +3,5 @@ import Adafruit_DHT
 sensor = Adafruit_DHT.DHT11
 pin = 5
 
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin, platform=Adafruit_DHT.DHT11)
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin, retries=5, delay_seconds=2)
+
