b274777f9373335ee83c18a2db998c20d3e2618f
nadia356 <110473332+nadia356@users.noreply.github.com>
2023-03-17 01:32:55 -0600

    t

--- Temperatura.py
+++ Temperatura.py
@@ -1,13 +1,9 @@
 import Adafruit_DHT
-Adafruit_DHT.platform = Adafruit_DHT.Raspberry_Pi_2
 
-
-# Configura el pin GPIO que está conectado al pin DATA del sensor
-sensor = Adafruit_DHT.DHT11
+sensor = Adafruit_DHT.DHT22
 pin = 5
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin, retries=10, delay_seconds=2)
 
-# Intenta leer la temperatura y humedad del sensor
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 # Si se pudo leer la temperatura y humedad, imprime los valores
 if humidity is not None and temperature is not None:
