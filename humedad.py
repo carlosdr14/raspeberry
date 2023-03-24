0d5b16c8048c6e1b3fcfb00fd74243350e77c303
nadia356 <110473332+nadia356@users.noreply.github.com>
2023-03-23 21:29:29 -0600

    ns

--- humedad.py
+++ humedad.py
@@ -1,18 +1,16 @@
-import Adafruit_DHT
- 
-# Set sensor type : Options are DHT11,DHT22 or AM2302
-sensor=Adafruit_DHT.DHT11
- 
-# Set GPIO sensor is connected to
-gpio=16
- 
-# Use read_retry method. This will retry up to 15 times to
-# get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
- 
-# Reading the DHT11 is very sensitive to timings and occasionally
-# the Pi might fail to get a valid reading. So check if readings are valid.
-if humidity is not None and temperature is not None:
-  print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-  print('Failed to get reading. Try again!')
+import time
+import board
+import adafruit_dht
+#Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT11(board.D17)
+while True:
+    try:
+         # Print the values to the serial port
+         temperature_c = dhtDevice.temperature
+         temperature_f = temperature_c * (9 / 5) + 32
+         humidity = dhtDevice.humidity
+         print("Temp: {:.1f} F / {:.1f} C    Humidity: {}% "
+               .format(temperature_f, temperature_c, humidity))
+    except RuntimeError as error:     # Errors happen fairly often, DHT's are hard to read, just keep going
+         print(error.args[0])
+    time.sleep(2.0)
