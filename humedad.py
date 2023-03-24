4699e8f373ad3adf973ff3e74c3ce08f0e415577
nadia356 <110473332+nadia356@users.noreply.github.com>
2023-03-23 21:26:38 -0600

    jd

--- humedad.py
+++ humedad.py
@@ -1,31 +1,18 @@
 import Adafruit_DHT
-import RPi.GPIO as GPIO
-import time
-
-# Configuración del pin GPIO
-GPIO.setmode(GPIO.BCM)
-DHT_PIN = 16 # El pin GPIO que se utiliza para el sensor DHT11
-
-# Función para leer la temperatura y la humedad del sensor
-def read_sensor():
-    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, DHT_PIN)
-    if humidity is not None and temperature is not None:
-        return temperature, humidity
-    else:
-        return None, None
-
-# Bucle principal del programa
-try:
-    while True:
-        temperature, humidity = read_sensor()
-        if temperature is not None and humidity is not None:
-            print('Temperatura={0:0.1f}°C  Humedad={1:0.1f}%'.format(temperature, humidity))
-        else:
-            print('Error al leer el sensor.')
-        time.sleep(2)  # Esperar 2 segundos antes de volver a leer el sensor
-
-# Detener el programa cuando se presiona Ctrl-C
-except KeyboardInterrupt:
-    print('Programa detenido por el usuario.')
-finally:
-    GPIO.cleanup()  # Liberar los recursos del pin GPIO
+ 
+# Set sensor type : Options are DHT11,DHT22 or AM2302
+sensor=Adafruit_DHT.DHT11
+ 
+# Set GPIO sensor is connected to
+gpio=16
+ 
+# Use read_retry method. This will retry up to 15 times to
+# get a sensor reading (waiting 2 seconds between each retry).
+humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
+ 
+# Reading the DHT11 is very sensitive to timings and occasionally
+# the Pi might fail to get a valid reading. So check if readings are valid.
+if humidity is not None and temperature is not None:
+  print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+else:
+  print('Failed to get reading. Try again!')
