9351e912ccb06d656b155c6e04bf578c56ce4ea5
nadia356 <110473332+nadia356@users.noreply.github.com>
2023-03-24 00:05:54 -0600

    d

--- humedad.py
+++ humedad.py
@@ -38,8 +38,7 @@ class DHTSensor(Lista, JSONHandler):
             db = client["Raspberry"]
             collection = db['Temperatura']
             print("Connected to MongoDB")
-            self.append(d)
-            self.save(d)
+          
             collection.insert_one(d)
         else:
             print(message)
